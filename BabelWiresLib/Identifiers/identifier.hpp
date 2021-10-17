d327164aa6fc458e6e34c244880875d8b8175c2c
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2021-10-17 08:25:19 +0100

    FieldNameRegistry -> IdentifierRegistry

--- BabelWiresLib/Identifiers/identifier.hpp
+++ BabelWiresLib/Identifiers/identifier.hpp
@@ -27,8 +27,10 @@ namespace babelwires {
     /// The string should be sufficient on its own to uniquely define an object during
     /// local usage (e.g. a field within a record, or an Enum value within an Enum).
     /// The discriminator is used to ensure the identifier is globally unique, which allows
-    /// the identifier to act as a key for look-up in a global registry. Global uniqueness
-    /// is achieved by registering the identifier with the IdentifierRegistry.
+    /// the identifier to act as a key for look-up in a global registry. 
+    ///
+    /// Global uniqueness is achieved by registering the identifier with the IdentifierRegistry.
+    /// The most convenient way of registering an identifier is to use the REGISTERED_ID macro.
     ///
     /// Data is arranged so the bit pattern can never be confused for array indices, when
     /// considering FeaturePaths. On little-endian architectures, the string is stored in
