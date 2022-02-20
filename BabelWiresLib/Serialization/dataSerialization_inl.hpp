8a6a8a28dcc33bd7bbbb020ca0a5a0f11a8d6c80
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2022-02-20 09:55:13 +0000

    DataSerialization

---
+++ BabelWiresLib/Serialization/dataSerialization_inl.hpp
@@ -0,0 +1,86 @@
+/**
+ * Functions for saving and loading data.
+ *
+ * (C) 2021 Malcolm Tyrrell
+ * 
+ * Licensed under the GPLv3.0. See LICENSE file.
+ **/
+#include "BabelWiresLib/Serialization/projectSerialization.hpp"
+#include "BabelWiresLib/Project/Modifiers/modifierData.hpp"
+#include "BabelWiresLib/Project/projectContext.hpp"
+#include "BabelWiresLib/Serialization/projectBundle.hpp"
+
+#include "Common/IO/outFileStream.hpp"
+#include "Common/Serialization/XML/xmlDeserializer.hpp"
+#include "Common/Serialization/XML/xmlSerializer.hpp"
+#include "Common/exceptions.hpp"
+
+#include <fstream>
+
+template <typename BUNDLE>
+typename babelwires::DataSerialization<BUNDLE>::Data babelwires::DataSerialization<BUNDLE>::loadFromStream(std::istream& is,
+                                                                         const ProjectContext& context,
+                                                                         const std::filesystem::path& pathToFile,
+                                                                         UserLogger& userLogger) {
+    std::string str((std::istreambuf_iterator<char>(is)), std::istreambuf_iterator<char>());
+
+    XmlDeserializer deserializer(str, context.m_deserializationReg, userLogger);
+    try {
+        auto projectBundle = deserializer.deserializeObject<BUNDLE>(BUNDLE::serializationType);
+        assert(projectBundle);
+        deserializer.finalize();
+        return std::move(*projectBundle).resolveAgainstCurrentContext(context, pathToFile, userLogger);
+    } catch (ParseException& e) {
+        deserializer.addContextDescription(e);
+        throw;
+    }
+}
+
+template <typename BUNDLE>
+typename babelwires::DataSerialization<BUNDLE>::Data babelwires::DataSerialization<BUNDLE>::loadFromFile(const std::filesystem::path& pathToFile,
+                                                                       const ProjectContext& context,
+                                                                       UserLogger& userLogger) {
+    std::ifstream is(pathToFile);
+    try {
+        return loadFromStream(is, context, pathToFile, userLogger);
+    } catch (ParseException& e) {
+        e << " when reading the file \"" << pathToFile << "\"";
+        throw;
+    }
+}
+
+template <typename BUNDLE>
+typename babelwires::DataSerialization<BUNDLE>::Data babelwires::DataSerialization<BUNDLE>::loadFromString(const std::string& string,
+                                                                         const ProjectContext& context,
+                                                                         const std::filesystem::path& pathToFile,
+                                                                         UserLogger& userLogger) {
+    std::istringstream is(string);
+    return loadFromStream(is, context, pathToFile, userLogger);
+}
+
+template <typename BUNDLE>
+void babelwires::DataSerialization<BUNDLE>::saveToStream(std::ostream& os, const std::filesystem::path& pathToFile, Data data) {
+    XmlSerializer serializer;
+    BUNDLE bundle(pathToFile, std::move(data));
+    bundle.interpretInCurrentContext();
+    serializer.serializeObject(bundle);
+    serializer.write(os);
+}
+
+template <typename BUNDLE>
+void babelwires::DataSerialization<BUNDLE>::saveToFile(const std::filesystem::path& pathToFile, Data data) {
+    try {
+        OutFileStream os(pathToFile);
+        saveToStream(os, pathToFile, std::move(data));
+        os.close();
+    } catch (const std::exception&) {
+        throw FileIoException() << "Failed to save the project to: " << pathToFile;
+    }
+}
+
+template <typename BUNDLE>
+std::string babelwires::DataSerialization<BUNDLE>::saveToString(const std::filesystem::path& pathToFile, Data data) {
+    std::ostringstream os;
+    saveToStream(os, pathToFile, std::move(data));
+    return os.str();
+}
--- BabelWiresLib/Serialization/projectSerialization.hpp
+++ BabelWiresLib/Serialization/projectSerialization.hpp
@@ -7,41 +7,10 @@
  **/
 #pragma once
 
-#include "BabelWiresLib/Project/projectData.hpp"
-
-#include <istream>
-#include <memory>
-#include <ostream>
-#include <filesystem>
+#include "BabelWiresLib/Serialization/dataSerialization.hpp"
+#include "BabelWiresLib/Serialization/projectBundle.hpp"
 
 namespace babelwires {
-    struct UserLogger;
-    struct ProjectContext;
-
     /// Isolates the codebase from the choice of format and other implementation details of serialization.
-    namespace ProjectSerialization {
-        ProjectData loadFromFile(const std::filesystem::path& pathToProjectFile, const ProjectContext& context, UserLogger& userLogger);
-
-        /// Throws a FileIoException on failure.
-        void saveToFile(const std::filesystem::path& pathToProjectFile, ProjectData projectData);
-
-        /// Load the projectData from the given string, resolving the data in the given context and pathToProjectFile.
-        /// pathToProjectFile may be empty, in which case only absolute file paths in the data can be resolved.
-        /// That might happen if data is being pasted into an unsaved project. Fortunately, this probably means that the
-        /// absolute paths originate in the current filesystem.
-        ProjectData loadFromString(const std::string& string, const ProjectContext& context, const std::filesystem::path& pathToProjectFile, UserLogger& userLogger);
-
-        /// Serialize the projectData and return it as a string.
-        /// pathToProjectFile is used to store relative versions of file paths.
-        /// pathToProjectFile may be empty, in which case only absolute file paths in the data will be stored.
-        /// That might happen if data is being copied from an unsaved project.
-        std::string saveToString(const std::filesystem::path& pathToProjectFile, ProjectData projectData);
-
-        namespace internal {
-            /// Load the projectData from is, resolving the data in the given context and pathToProjectFile.
-            ProjectData loadFromStream(std::istream& is, const ProjectContext& context, const std::filesystem::path& pathToProjectFile, UserLogger& userLogger);
-
-            void saveToStream(std::ostream& os, const std::filesystem::path& pathToProjectFile, ProjectData projectData);
-        }
-    } // namespace ProjectSerialization
+    class ProjectSerialization : public DataSerialization<ProjectBundle> {};
 } // namespace babelwires
