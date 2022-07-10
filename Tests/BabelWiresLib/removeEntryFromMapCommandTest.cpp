791a48094fc0cbaf8e40dbb7f6a1f851f99ede99
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2022-07-10 09:37:16 +0100

    Replace remove invalid

--- Tests/BabelWiresLib/removeEntryFromMapCommandTest.cpp
+++ Tests/BabelWiresLib/removeEntryFromMapCommandTest.cpp
@@ -63,6 +63,55 @@ TEST(RemoveEntryFromMapCommandTest, executeAndUndo) {
     EXPECT_EQ(mapProject.getMapEntry(2).getData().getKind(), babelwires::MapEntryData::Kind::AllToOne);
 }
 
+TEST(RemoveEntryFromMapCommandTest, removeInvalid) {
+    babelwires::IdentifierRegistryScope identifierRegistry;
+    testUtils::TestEnvironment environment;
+    environment.m_typeSystem.addEntry(std::make_unique<testUtils::TestType>());
+
+    babelwires::MapProject mapProject(environment.m_projectContext);
+    mapProject.setAllowedSourceTypeId(testUtils::TestType::getThisIdentifier());
+    mapProject.setAllowedTargetTypeId(testUtils::TestType::getThisIdentifier());
+
+    babelwires::MapData mapData;
+    mapData.setSourceTypeId(testUtils::TestType::getThisIdentifier());
+    mapData.setTargetTypeId(testUtils::TestType::getThisIdentifier());
+
+    babelwires::OneToOneMapEntryData oneToOne(environment.m_typeSystem, testUtils::TestType::getThisIdentifier(),
+                                              testUtils::TestType::getThisIdentifier());
+
+    babelwires::AllToOneFallbackMapEntryData allToOne(environment.m_typeSystem,
+                                                       testUtils::TestType::getThisIdentifier());
+
+    mapData.emplaceBack(oneToOne.clone());
+    mapData.emplaceBack(allToOne.clone());
+    mapData.emplaceBack(allToOne.clone());
+    mapProject.setMapData(mapData);
+
+    EXPECT_EQ(mapProject.getNumMapEntries(), 3);
+    EXPECT_TRUE(mapProject.getMapEntry(0).getValidity());
+    EXPECT_FALSE(mapProject.getMapEntry(1).getValidity());
+    EXPECT_TRUE(mapProject.getMapEntry(2).getValidity());
+
+    babelwires::RemoveEntryFromMapCommand command("Remove", 1);
+    
+    EXPECT_TRUE(command.initialize(mapProject));
+    command.execute(mapProject);
+
+    EXPECT_EQ(mapProject.getNumMapEntries(), 2);
+    EXPECT_EQ(mapProject.getMapEntry(0).getData().getKind(), babelwires::MapEntryData::Kind::OneToOne);
+    EXPECT_EQ(mapProject.getMapEntry(1).getData().getKind(), babelwires::MapEntryData::Kind::AllToOne);
+    EXPECT_TRUE(mapProject.getMapEntry(0).getValidity());
+    EXPECT_TRUE(mapProject.getMapEntry(1).getValidity());
+
+    command.undo(mapProject);
+    EXPECT_EQ(mapProject.getMapEntry(0).getData().getKind(), babelwires::MapEntryData::Kind::OneToOne);
+    EXPECT_EQ(mapProject.getMapEntry(1).getData().getKind(), babelwires::MapEntryData::Kind::AllToOne);
+    EXPECT_EQ(mapProject.getMapEntry(2).getData().getKind(), babelwires::MapEntryData::Kind::AllToOne);
+    EXPECT_TRUE(mapProject.getMapEntry(0).getValidity());
+    EXPECT_FALSE(mapProject.getMapEntry(1).getValidity());
+    EXPECT_TRUE(mapProject.getMapEntry(2).getValidity());
+}
+
 TEST(RemoveEntryFromMapCommandTest, failAtEnd) {
     babelwires::IdentifierRegistryScope identifierRegistry;
     testUtils::TestEnvironment environment;
--- Tests/BabelWiresLib/replaceMapEntryCommandTest.cpp
+++ Tests/BabelWiresLib/replaceMapEntryCommandTest.cpp
@@ -97,6 +97,66 @@ TEST(ReplaceMapEntryCommandTest, failBeyondEnd) {
     EXPECT_FALSE(command.initialize(mapProject));    
 }
 
+TEST(ReplaceMapEntryCommandTest, replaceInvalid) {
+    babelwires::IdentifierRegistryScope identifierRegistry;
+    testUtils::TestEnvironment environment;
+    environment.m_typeSystem.addEntry(std::make_unique<testUtils::TestType>());
+
+    babelwires::MapProject mapProject(environment.m_projectContext);
+    mapProject.setAllowedSourceTypeId(testUtils::TestType::getThisIdentifier());
+    mapProject.setAllowedTargetTypeId(testUtils::TestType::getThisIdentifier());
+
+    babelwires::MapData mapData;
+    mapData.setSourceTypeId(testUtils::TestType::getThisIdentifier());
+    mapData.setTargetTypeId(testUtils::TestType::getThisIdentifier());
+
+    babelwires::OneToOneMapEntryData oneToOne(environment.m_typeSystem, testUtils::TestType::getThisIdentifier(),
+                                              testUtils::TestType::getThisIdentifier());
+
+    babelwires::AllToOneFallbackMapEntryData allToOne(environment.m_typeSystem,
+                                                       testUtils::TestType::getThisIdentifier());
+
+    mapData.emplaceBack(oneToOne.clone());
+    mapData.emplaceBack(allToOne.clone());
+    mapData.emplaceBack(allToOne.clone());
+    mapProject.setMapData(mapData);
+
+    EXPECT_TRUE(mapProject.getMapEntry(0).getValidity());
+    EXPECT_FALSE(mapProject.getMapEntry(1).getValidity());
+    EXPECT_TRUE(mapProject.getMapEntry(2).getValidity());
+
+    babelwires::OneToOneMapEntryData oneToOne2(environment.m_typeSystem, testUtils::TestType::getThisIdentifier(),
+                                              testUtils::TestType::getThisIdentifier());
+    testUtils::TestValue newSourceValue;
+    testUtils::TestValue newTargetValue;
+    newSourceValue.m_value = "Source2";
+    newTargetValue.m_value = "Target2";
+    oneToOne2.setSourceValue(newSourceValue.clone());
+    oneToOne2.setTargetValue(newTargetValue.clone());
+    
+    babelwires::ReplaceMapEntryCommand command("Replace", oneToOne2.clone(), 1);
+    
+    EXPECT_TRUE(command.initialize(mapProject));
+    command.execute(mapProject);
+
+    EXPECT_EQ(mapProject.getNumMapEntries(), 3);
+    EXPECT_EQ(mapProject.getMapEntry(0).getData().getKind(), babelwires::MapEntryData::Kind::OneToOne);
+    EXPECT_EQ(mapProject.getMapEntry(1).getData().getKind(), babelwires::MapEntryData::Kind::OneToOne);
+    EXPECT_EQ(mapProject.getMapEntry(1).getData(), oneToOne2);
+    EXPECT_EQ(mapProject.getMapEntry(2).getData().getKind(), babelwires::MapEntryData::Kind::AllToOne);
+    EXPECT_TRUE(mapProject.getMapEntry(0).getValidity());
+    EXPECT_TRUE(mapProject.getMapEntry(1).getValidity());
+    EXPECT_TRUE(mapProject.getMapEntry(2).getValidity());
+
+    command.undo(mapProject);
+    EXPECT_EQ(mapProject.getMapEntry(0).getData().getKind(), babelwires::MapEntryData::Kind::OneToOne);
+    EXPECT_EQ(mapProject.getMapEntry(1).getData().getKind(), babelwires::MapEntryData::Kind::AllToOne);
+    EXPECT_EQ(mapProject.getMapEntry(2).getData().getKind(), babelwires::MapEntryData::Kind::AllToOne);
+    EXPECT_TRUE(mapProject.getMapEntry(0).getValidity());
+    EXPECT_FALSE(mapProject.getMapEntry(1).getValidity());
+    EXPECT_TRUE(mapProject.getMapEntry(2).getValidity());
+}
+
 TEST(ReplaceMapEntryCommandTest, failInvalid) {
     babelwires::IdentifierRegistryScope identifierRegistry;
     testUtils::TestEnvironment environment;
