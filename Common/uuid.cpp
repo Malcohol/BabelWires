93970f88c104ac5f9f3043e4370d23cefa1d654a
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2023-02-07 20:05:59 +0000

    Reimplement UUID using uint

--- Common/uuid.cpp
+++ Common/uuid.cpp
@@ -18,32 +18,31 @@ namespace {
     const char zeroPattern[] = "00000000-0000-0000-0000-000000000000";
 }
 
-babelwires::Uuid::Uuid()
-    : m_text(zeroPattern) {
-}
+babelwires::Uuid::Uuid() = default;
 
-babelwires::Uuid::Uuid(std::string uuidText)
-    : m_text(std::move(uuidText)) {
-    assert(isValid(m_text) && "The provided UUID was not in the correct form.");
-    // It's not required of the input, but apparently, UUIDs should be written in lower case digits.
-    std::transform(m_text.begin(), m_text.end(), m_text.begin(), [](unsigned char c) { return std::tolower(c); });
+babelwires::Uuid::Uuid(const std::string& uuidText) {
+    assert(isValid(uuidText) && "The provided UUID was not in the correct form.");
+    {
+        std::stringstream tmp;
+        tmp << uuidText.substr(0, 8) << uuidText.substr(9, 4) << uuidText.substr(14, 4);
+        tmp >> std::hex >> m_high;
+    }
+    {
+        std::stringstream tmp;
+        tmp << uuidText.substr(19, 4) << uuidText.substr(24, 12);
+        tmp >> std::hex >> m_low;
+    }
 }
 
 bool babelwires::Uuid::isZero() const {
-    return m_text == zeroPattern;
+    return (m_low == 0) && (m_high == 0);
 }
 
 void babelwires::Uuid::randomize(std::default_random_engine& randomEngine) {
     do {
-        std::ostringstream os;
-        for (unsigned int i = 0; i < 8; ++i) {
-            if ((i >= 2) && (i <= 5)) {
-                os << "-";
-            }
-            os << std::hex << std::setw(4) << std::setfill('0') << std::uniform_int_distribution<std::uint16_t>()(randomEngine);
-        }
-        m_text = os.str();
-    } while (m_text == zeroPattern);
+        m_high = std::uniform_int_distribution<std::uint64_t>()(randomEngine);
+        m_low = std::uniform_int_distribution<std::uint64_t>()(randomEngine);
+    } while (isZero());
 }
 
 babelwires::Uuid babelwires::Uuid::deserializeFromString(std::string_view uuidText) {
@@ -52,8 +51,6 @@ babelwires::Uuid babelwires::Uuid::deserializeFromString(std::string_view uuidTe
     }
     return Uuid(std::string(uuidText));
 }
-
-// xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
 
 bool babelwires::Uuid::isValid(std::string_view uuidText) {
     constexpr unsigned char hyphenIndices[] = {8, 13, 18, 23, 255};
