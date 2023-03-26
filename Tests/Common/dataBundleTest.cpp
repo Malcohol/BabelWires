8503554e0e4f84b2732aeb7ed5000e8bc6de4637
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2023-03-26 15:16:29 +0100

    DataBundleTest filePathResolution

--- Tests/Common/dataBundleTest.cpp
+++ Tests/Common/dataBundleTest.cpp
@@ -6,6 +6,7 @@
 #include <Common/Serialization/deserializer.hpp>
 #include <Common/Serialization/serializer.hpp>
 
+#include <Tests/TestUtils/tempFilePath.hpp>
 #include <Tests/TestUtils/testLog.hpp>
 
 namespace {
@@ -58,11 +59,9 @@ namespace {
     struct TestBundle : babelwires::DataBundle<TestBundlePayload> {
         SERIALIZABLE(TestBundle, "TestBundle", void, 1);
 
-
         TestBundle() = default;
         TestBundle(std::filesystem::path pathToBundleFile, TestBundlePayload&& payload)
-            : DataBundle(std::move(pathToBundleFile), std::move(payload)) {
-        }
+            : DataBundle(std::move(pathToBundleFile), std::move(payload)) {}
 
         void visitIdentifiers(babelwires::IdentifierVisitor& visitor) override { getData().visitIdentifiers(visitor); }
 
@@ -80,7 +79,7 @@ namespace {
     };
 } // namespace
 
-TEST(ProjectBundleTest, identifiers) {
+TEST(DataBundleTest, identifiers) {
     // This will carry data between the first part of the test and the second.
     TestBundle bundle;
 
@@ -102,14 +101,14 @@ TEST(ProjectBundleTest, identifiers) {
         TestBundlePayload sourcePayload(15);
 
         EXPECT_EQ(sourcePayload.m_shortId.getDiscriminator(), 1);
-        EXPECT_EQ(sourcePayload.m_mediumId.getDiscriminator(), 2); 
+        EXPECT_EQ(sourcePayload.m_mediumId.getDiscriminator(), 2);
         EXPECT_EQ(sourcePayload.m_longId.getDiscriminator(), 3);
 
         bundle = TestBundle(std::filesystem::current_path(), std::move(sourcePayload));
         bundle.interpretInCurrentContext();
 
         EXPECT_EQ(bundle.getData().m_shortId.getDiscriminator(), 1);
-        EXPECT_EQ(bundle.getData().m_mediumId.getDiscriminator(), 1); 
+        EXPECT_EQ(bundle.getData().m_mediumId.getDiscriminator(), 1);
         EXPECT_EQ(bundle.getData().m_longId.getDiscriminator(), 1);
     }
 
@@ -134,7 +133,76 @@ TEST(ProjectBundleTest, identifiers) {
             std::move(bundle).resolveAgainstCurrentContext(dataContext, std::filesystem::current_path(), testLog);
 
         EXPECT_EQ(targetPayload.m_shortId.getDiscriminator(), 3);
-        EXPECT_EQ(targetPayload.m_mediumId.getDiscriminator(), 2); 
+        EXPECT_EQ(targetPayload.m_mediumId.getDiscriminator(), 2);
         EXPECT_EQ(targetPayload.m_longId.getDiscriminator(), 1);
+    }
+}
+
+TEST(DataBundleTest, filePathResolution) {
+    testUtils::TestLog log;
+
+    std::filesystem::path root = std::filesystem::canonical(std::filesystem::temp_directory_path());
+
+    testUtils::TempDirectory FooBar("Foo/Bar");
+    testUtils::TempDirectory OomBar("Oom/Bar");
+
+    testUtils::TempFilePath fileWhichExists("Foo/Bar/Bar.boo");
+    fileWhichExists.ensureExists();
+
+    testUtils::TempFilePath fileWhichExists2("Oom/Bar/Bar.boo");
+    fileWhichExists2.ensureExists();
+
+    struct TestScenario {
+        std::filesystem::path m_pathInProjectData;
+        std::filesystem::path m_newBase;
+        std::filesystem::path m_oldBase;
+        std::filesystem::path m_expectedResolvedPath;
+    };
+
+    // Based on the tests in FilePathTest.
+    // Because we interpret / resolve on the same platform, some of those tests do not carry over.
+    std::vector<TestScenario> scenarios = {
+        // Same testEnvironment.
+        TestScenario{root / "Foo/Bar/Bar.boo", root / "Foo/Bar", root / "Foo/Bar", root / "Foo/Bar/Bar.boo"},
+        // Same testEnvironment.
+        TestScenario{root / "Foo/Bar/Bar.boo", root / "Foo", root / "Foo", root / "Foo/Bar/Bar.boo"},
+        // Same testEnvironment, file doesn't exist.
+        TestScenario{root / "Flerm/Bar/Bar.boo", root / "Flerm", root / "Flerm", root / "Flerm/Bar/Bar.boo"},
+        // New exists, old doesn't.
+        TestScenario{root / "Foo/Bar/Bar.boo", root / "Foo", root / "Flerm", root / "Foo/Bar/Bar.boo"},
+        // Old exists, new doesn't.
+        TestScenario{root / "Foo/Bar/Bar.boo", root / "Foo", root / "Flerm", root / "Foo/Bar/Bar.boo"},
+        // Both exist. New prefered. Check log.
+        TestScenario{root / "Foo/Bar/Bar.boo", root / "Foo", root / "Oom", root / "Foo/Bar/Bar.boo"},
+        // Empty new
+        TestScenario{root / "Foo/Bar/Bar.boo", std::filesystem::path(), root / "Foo", root / "Foo/Bar/Bar.boo"},
+        // Empty old
+        TestScenario{root / "Foo/Bar/Bar.boo", root / "Foo", std::filesystem::path(), root / "Foo/Bar/Bar.boo"}};
+
+    // Check that the files in the element data get resolved in the expected way were the project to be opened
+    // in a different place.
+    for (const auto& scenario : scenarios) {
+        TestBundle bundle;
+        {
+            testUtils::TestLog testLog;
+            babelwires::IdentifierRegistryScope identifierRegistry;
+
+            TestBundlePayload projectData(14);
+            projectData.m_filePath = scenario.m_pathInProjectData;
+            bundle = TestBundle(scenario.m_oldBase, std::move(projectData));
+            bundle.interpretInCurrentContext();
+        }
+
+        {
+            testUtils::TestLog testLog;
+            babelwires::IdentifierRegistryScope identifierRegistry;
+            babelwires::AutomaticDeserializationRegistry deserializationReg;
+            babelwires::DataContext dataContext{deserializationReg};
+
+            TestBundlePayload projectData;
+            projectData = std::move(bundle).resolveAgainstCurrentContext(dataContext, scenario.m_newBase, log);
+
+            EXPECT_EQ(projectData.m_filePath, scenario.m_expectedResolvedPath);
+        }
     }
 }
