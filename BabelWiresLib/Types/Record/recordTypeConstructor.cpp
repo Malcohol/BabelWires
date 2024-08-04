994094238796fd22b6496f0040cd63fab87fb010
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2024-08-04 15:31:04 +0100

    Add RecordTypeConstructor

---
+++ BabelWiresLib/Types/Record/recordTypeConstructor.cpp
@@ -0,0 +1,32 @@
+/**
+ * 
+ *
+ * (C) 2021 Malcolm Tyrrell
+ *
+ * Licensed under the GPLv3.0. See LICENSE file.
+ **/
+#include <BabelWiresLib/Types/Record/recordTypeConstructor.hpp>
+
+#include <BabelWiresLib/TypeSystem/typeSystemException.hpp>
+#include <BabelWiresLib/Types/Record/recordType.hpp>
+
+
+std::unique_ptr<babelwires::Type>
+babelwires::RecordTypeConstructor::constructType(const TypeSystem& typeSystem, TypeRef newTypeRef, const std::vector<const Type*>& typeArguments,
+                                              const std::vector<EditableValueHolder>& valueArguments) const {
+    if (typeArguments.size() != valueArguments.size()) {
+        throw TypeSystemException() << "RecordTypeConstructor requires the same number of types and values, but got "
+                                    << typeArguments.size() << " and " << valueArguments.size() << " respectively";
+    }
+
+    std::vector<RecordType::Field> fields;
+    for (unsigned int i = 0; i < valueArguments.size(); ++i) {
+        if (const FieldIdValue* fieldId = valueArguments[i]->as<FieldIdValue>()) {
+            fields.emplace_back(RecordType::Field{fieldId->get(), typeArguments[i]->getTypeRef()});
+        } else {
+            throw TypeSystemException() << "RecordTypeConstructor value argument " << i << " was not a FieldIdValue";
+        }
+    }
+
+    return std::make_unique<ConstructedType<RecordType>>(std::move(newTypeRef), std::move(fields));
+}
