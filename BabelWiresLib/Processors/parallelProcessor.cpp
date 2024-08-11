3373d5c231548c2541f2027f73644b66f16e4217
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2024-08-11 18:15:30 +0100

    Remove the old ParallelProcessor

--- BabelWiresLib/Processors/parallelProcessor.cpp
+++ BabelWiresLib/Processors/parallelProcessor.cpp
@@ -23,59 +23,6 @@
 #include <array>
 #include <execution>
 
-void babelwires::Detail::parallelProcessorHelper(
-    const RecordFeature* inputFeature, const babelwires::ArrayFeature* arrayIn, babelwires::ArrayFeature* arrayOut,
-    babelwires::UserLogger& userLogger,
-    std::function<void(babelwires::UserLogger&, const Feature*, Feature*)> processEntry) {
-    if (arrayIn->isChanged(babelwires::Feature::Changes::StructureChanged)) {
-        // Keep the out structure in sync with the in structure, and
-        // try to ensure that tracks which correspond before still
-        // correspond afterwards.
-        arrayOut->copyStructureFrom(*arrayIn);
-    }
-
-    bool nonArrayInputChange = false;
-    for (auto i : subfeatures(inputFeature)) {
-        if ((i != arrayIn) && i->isChanged(Feature::Changes::SomethingChanged)) {
-            nonArrayInputChange = true;
-            break;
-        }
-    }
-
-    std::array<int, s_maxParallelFeatures> indices;
-    bool isFailed = false;
-    std::array<std::string, s_maxParallelFeatures> failureStrings;
-    std::iota(indices.begin(), indices.end(), 0);
-    std::for_each(
-#ifndef __APPLE__
-        std::execution::par,
-#endif
-        indices.begin(), indices.begin() + arrayIn->getNumFeatures(),
-                  [&arrayIn, &arrayOut, &userLogger, &failureStrings, &isFailed, &processEntry, nonArrayInputChange](int i) {
-                      const Feature* inputEntry = arrayIn->getFeature(i);
-                      if (nonArrayInputChange || inputEntry->isChanged(Feature::Changes::SomethingChanged)) {
-                        try {
-                            processEntry(userLogger, inputEntry, arrayOut->getFeature(i));
-                        } catch (const BaseException& e) {
-                            failureStrings[i] = e.what();
-                            isFailed = true;
-                        }
-                      }
-                  });
-    if (isFailed) {
-        // TODO: Need a more precise way to signal failure.
-        ModelException compositeException;
-        const char* newline = "";
-        for (int i = 0; i < failureStrings.size(); ++i) {
-            if (!failureStrings[i].empty()) {
-                compositeException << newline << "Failure processing entry " << FeaturePath(arrayIn->getFeature(i)) << ": " << failureStrings[i];
-                newline = "\n";
-            }
-        }
-        throw compositeException;
-    }
-}
-
 namespace {
     babelwires::TypeRef getParallelArray(babelwires::TypeRef&& entryType) {
         return babelwires::TypeRef{
--- BabelWiresLib/Processors/parallelProcessor.hpp
+++ BabelWiresLib/Processors/parallelProcessor.hpp
@@ -19,59 +19,6 @@
 namespace babelwires {
     constexpr int s_maxParallelFeatures = 16;
 
-    namespace Detail {
-        /// This helper function carries code which doesn't need to be templated.
-        void parallelProcessorHelper(const RecordFeature* inputFeature, const ArrayFeature* arrayIn,
-                                     ArrayFeature* arrayOut, UserLogger& userLogger,
-                                     std::function<void(UserLogger&, const Feature*, Feature*)> processEntry);
-    } // namespace Detail
-
-    /// A base class for a common shape of processor which performs the same operation on several input features,
-    /// producing several output features. Organizing suitable processors this way should reduce the number of
-    /// elements in the project.
-    ///
-    /// Subclasses should call addArrayFeature in their constructor, and need to override the pure virtual processEntry
-    /// method.
-    template <typename INPUT_ENTRY_FEATURE, typename OUTPUT_ENTRY_FEATURE>
-    class ParallelProcessor : public CommonProcessor {
-      public:
-        ParallelProcessor(const ProjectContext& projectContext)
-            : CommonProcessor(projectContext) {}
-
-        using InputEntryFeature = INPUT_ENTRY_FEATURE;
-        using OutputEntryFeature = OUTPUT_ENTRY_FEATURE;
-        using InputArrayFeature = HasStaticSizeRange<StandardArrayFeature<InputEntryFeature>, 1, s_maxParallelFeatures>;
-        using OutputArrayFeature =
-            HasStaticSizeRange<StandardArrayFeature<OutputEntryFeature>, 1, s_maxParallelFeatures>;
-
-        void process(UserLogger& userLogger) override {
-            Detail::parallelProcessorHelper(m_inputFeature.get(), m_arrayIn, m_arrayOut, userLogger,
-                                            [this](UserLogger& userLogger, const Feature* input, Feature* output) {
-                                                const auto& featureIn = input->is<InputEntryFeature>();
-                                                auto& featureOut = output->is<OutputEntryFeature>();
-                                                processEntry(userLogger, featureIn, featureOut);
-                                            });
-        }
-
-      protected:
-        /// Subclass constructors should call this to add the input/output array. This should almost always
-        /// be after all the input features are added.
-        void addArrayFeature(ShortId id) {
-            m_arrayIn = m_inputFeature->addField(std::make_unique<InputArrayFeature>(), id);
-            m_arrayOut = m_outputFeature->addField(std::make_unique<OutputArrayFeature>(), id);
-        }
-
-        /// Subclasses must override this with the per-entry processing logic. This is only called if
-        /// input has changed or some other input feature which isn't the array has changed.
-        /// This method can signal errors by throwing an exception.
-        virtual void processEntry(UserLogger& userLogger, const InputEntryFeature& input,
-                                  OutputEntryFeature& output) const = 0;
-
-      private:
-        ArrayFeature* m_arrayIn = nullptr;
-        ArrayFeature* m_arrayOut = nullptr;
-    };
-
     /// ParallelProcessors should override this for their input type. An array of the right shape will be automatically
     /// added at the end of the field set. It is typical (but not required) for parallel processors have common input
     /// settings separate from the per-entry inputs. A subclass can use the Instance DSL to provide convenient access
