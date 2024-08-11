ff7bdc954883cdc3c071f8dc5d4318e3c98bf7e5
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2024-08-11 12:07:34 +0100

    Add comments to ParallelValueProcessor

--- BabelWiresLib/Processors/parallelProcessor.hpp
+++ BabelWiresLib/Processors/parallelProcessor.hpp
@@ -72,16 +72,30 @@ namespace babelwires {
         ArrayFeature* m_arrayOut = nullptr;
     };
 
+    /// ParallelProcessors should override this for their input type. An array of the right shape will be automatically
+    /// added at the end of the field set. It is typical (but not required) for parallel processors have common input
+    /// settings separate from the per-entry inputs. A subclass can use the Instance DSL to provide convenient access
+    /// to the fields of the type. It should not be necessary to reference the array in the instance, since the
+    /// processor's ProcessEntry call provides direct access.
     class ParallelValueProcessorInputBase : public RecordType {
       public:
         ParallelValueProcessorInputBase(std::vector<RecordType::Field> commonInput, ShortId arrayId, TypeRef entryType);
     };
 
+    /// ParallelProcessors should override this for their output type. The type will contain an array of the right
+    /// shape.
+    /// Note: There is currently no support for parallel processors to generate some common output in addition to the
+    /// per-entry outputs. Support could be added if a use-case arises.
+    /// TODO: We could automatically create these output types using a RecordTypeConstructor. However, such a type could
+    /// not be directly addressed in a versioning system. Have a think about whether that matters.
     class ParallelValueProcessorOutputBase : public RecordType {
       public:
         ParallelValueProcessorOutputBase(ShortId arrayId, TypeRef entryType);
     };
 
+    /// A base class for a common shape of processor which performs the same operation on several input features,
+    /// producing several output features. Organizing suitable processors this way should reduce the number of
+    /// elements in the project.
     class ParallelValueProcessor : public ValueProcessor {
       public:
         ParallelValueProcessor(const ProjectContext& projectContext, const TypeRef& parallelInput,
