f005b187b6f9264efd568c2ce39cf924f81f3ec7
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2024-10-28 08:45:49 +0000

    Rename some parameters

--- BabelWiresLib/Instance/instanceUtils.hpp
+++ BabelWiresLib/Instance/instanceUtils.hpp
@@ -14,28 +14,28 @@ namespace babelwires {
     namespace InstanceUtils {
         // Utility functions common between RecordTypes and RecordWithVariantTypes
 
-        const babelwires::ValueTreeNode& getChild(const babelwires::ValueTreeNode& recordFeature, babelwires::ShortId id);
-        babelwires::ValueTreeNode& getChild(babelwires::ValueTreeNode& recordFeature, babelwires::ShortId id);
+        const babelwires::ValueTreeNode& getChild(const babelwires::ValueTreeNode& recordTreeNode, babelwires::ShortId id);
+        babelwires::ValueTreeNode& getChild(babelwires::ValueTreeNode& recordTreeNode, babelwires::ShortId id);
 
         // Utility functions for RecordTypes
 
-        const babelwires::ValueTreeNode* tryGetChild(const babelwires::ValueTreeNode& recordFeature,
+        const babelwires::ValueTreeNode* tryGetChild(const babelwires::ValueTreeNode& recordTreeNode,
                                                     babelwires::ShortId id);
-        babelwires::ValueTreeNode& activateAndGetChild(babelwires::ValueTreeNode& recordFeature, babelwires::ShortId id);
-        void deactivateChild(babelwires::ValueTreeNode& recordFeature, babelwires::ShortId id);
+        babelwires::ValueTreeNode& activateAndGetChild(babelwires::ValueTreeNode& recordTreeNode, babelwires::ShortId id);
+        void deactivateChild(babelwires::ValueTreeNode& recordTreeNode, babelwires::ShortId id);
 
         // Utility functions for RecordWithVariantTypes
 
-        ShortId getSelectedTag(const ValueTreeNode& valueFeature);
-        void selectTag(ValueTreeNode& valueFeature, ShortId tag);
+        ShortId getSelectedTag(const ValueTreeNode& valueTreeNode);
+        void selectTag(ValueTreeNode& valueTreeNode, ShortId tag);
 
         // Utility functions for ArrayTypes
 
-        unsigned int getArraySize(const ValueTreeNode& arrayFeature);
-        void setArraySize(ValueTreeNode& arrayFeature, unsigned int newSize);
+        unsigned int getArraySize(const ValueTreeNode& arrayTreeNode);
+        void setArraySize(ValueTreeNode& arrayTreeNode, unsigned int newSize);
 
-        const ValueTreeNode& getChild(const ValueTreeNode& arrayFeature, unsigned int index);
-        ValueTreeNode& getChild(ValueTreeNode& arrayFeature, unsigned int index);
+        const ValueTreeNode& getChild(const ValueTreeNode& arrayTreeNode, unsigned int index);
+        ValueTreeNode& getChild(ValueTreeNode& arrayTreeNode, unsigned int index);
     } // namespace InstanceUtils
 
 } // namespace babelwires
