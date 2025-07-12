0a59623120a88dad6b3ea7053bd1e82e1753c98f
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2025-07-12 11:09:36 +0100

    Fix after renames

--- Domains/TestDomain/testGenericType.hpp
+++ Domains/TestDomain/testGenericType.hpp
@@ -1,11 +1,11 @@
 #include <BabelWiresLib/Types/Generic/genericType.hpp>
 
-#include <BabelWiresLib/TypeSystem/primitiveType.hpp>
+#include <BabelWiresLib/TypeSystem/registeredType.hpp>
 
 namespace testDomain {
 
     class TestGenericType : public babelwires::GenericType {
       public:
-        PRIMITIVE_TYPE("TGen", "Test Generic Type", "80013367-d279-4f9d-aa38-6c397b31d38d", 1);
+        REGISTERED_TYPE("TGen", "Test Generic Type", "80013367-d279-4f9d-aa38-6c397b31d38d", 1);
     };
 } // namespace testDomain
