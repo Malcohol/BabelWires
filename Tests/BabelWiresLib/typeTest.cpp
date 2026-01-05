8227494e61e925f1aa661e32eeced27e37489f46
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2026-01-05 21:38:06 +0000

    Add a unit test for TypePtr

--- Tests/BabelWiresLib/typeTest.cpp
+++ Tests/BabelWiresLib/typeTest.cpp
@@ -3,16 +3,20 @@
 #include <BabelWiresLib/TypeSystem/typeSystem.hpp>
 #include <BabelWiresLib/Types/String/stringType.hpp>
 #include <BabelWiresLib/Types/String/stringValue.hpp>
+#include <BabelWiresLib/Types/Array/arrayTypeConstructor.hpp>
 
 #include <Domains/TestDomain/testEnum.hpp>
 
 #include <Tests/TestUtils/equalSets.hpp>
+#include <Tests/TestUtils/testLog.hpp>
 
 #include <Common/Identifiers/identifierRegistry.hpp>
 
 TEST(TypeTest, typeAndValue)
 {
     babelwires::TypeSystem typeSystem;
+    testUtils::TestLog log;
+
     // This mostly just exercises the API.
 
     babelwires::StringType type;
@@ -23,4 +27,38 @@ TEST(TypeTest, typeAndValue)
 
     testDomain::TestEnum testEnum;
     EXPECT_FALSE(testEnum.isValidValue(typeSystem, value));
+}
+
+TEST(TypeTest, typePtrTest)
+{
+    babelwires::TypeSystem typeSystem;
+    testUtils::TestLog log;
+
+    typeSystem.addEntry<babelwires::StringType>();
+    typeSystem.addTypeConstructor<babelwires::ArrayTypeConstructor>();
+
+    babelwires::TypeExp arrayOfStringsExp = babelwires::ArrayTypeConstructor::makeTypeExp(
+        babelwires::TypeExp(babelwires::StringType::getThisIdentifier()), 0, 10); 
+
+    babelwires::WeakTypePtr weakTypePtr;
+    {
+        babelwires::TypePtr typePtr = arrayOfStringsExp.resolve(typeSystem);
+        EXPECT_TRUE(typePtr);
+        babelwires::TypePtr typePtr2 = arrayOfStringsExp.resolve(typeSystem);
+        // Constructing the same type twice gives the same pointer.
+        EXPECT_EQ(typePtr, typePtr2);
+        weakTypePtr = typePtr;
+        EXPECT_FALSE(weakTypePtr.expired());
+    }
+    // Confirm TypeConstructor caches do not keep types alive longer than necessary.
+    EXPECT_TRUE(weakTypePtr.expired());
+
+    {
+        // Confirm that the type can be recreated.
+        babelwires::TypePtr typePtr = arrayOfStringsExp.resolve(typeSystem);
+        EXPECT_TRUE(typePtr);
+        weakTypePtr = typePtr;
+        EXPECT_FALSE(weakTypePtr.expired());
+    }
+    EXPECT_TRUE(weakTypePtr.expired());
 }
