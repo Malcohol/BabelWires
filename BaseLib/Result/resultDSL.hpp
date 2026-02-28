8cb6fac096dcbf89d3edc355b4ad0f78dd73d567
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2026-02-28 16:37:22 +0000

    Small result tidy.

--- BaseLib/Result/resultDSL.hpp
+++ BaseLib/Result/resultDSL.hpp
@@ -11,32 +11,8 @@
 // Assume that code that needs these macros needs wants Error too.
 #include <BaseLib/Result/error.hpp>
 
-// Helper macros
-
-#define BW_UNIQUE_NAME(X, Y) BW_COMBINE_HELPER(X, Y)
-#define BW_COMBINE_HELPER(X, Y) X##Y
-#define BW_CONCAT(X, Y) BW_COMBINE_HELPER(X, Y)
-
-// Argument counting for variadic ASSIGN_OR_ERROR dispatch.
-#define BW_ARG_COUNT_IMPL(_1, _2, _3, _4, _5, _6, N, ...) N
-#define BW_ARG_COUNT(...) BW_ARG_COUNT_IMPL(__VA_ARGS__, 6, 5, 4, 3, 2, 1)
-
-// Dispatch macros: extract the last argument as the expression, reassemble the rest as the target.
-#define BW_ASSIGN_2(a, expr) BW_ASSIGN_IMPL(expr, a)
-#define BW_ASSIGN_3(a, b, expr) BW_ASSIGN_IMPL(expr, a, b)
-#define BW_ASSIGN_4(a, b, c, expr) BW_ASSIGN_IMPL(expr, a, b, c)
-#define BW_ASSIGN_5(a, b, c, d, expr) BW_ASSIGN_IMPL(expr, a, b, c, d)
-#define BW_ASSIGN_6(a, b, c, d, e, expr) BW_ASSIGN_IMPL(expr, a, b, c, d, e)
-
-// We can't use a scope in this case since it would enclose the target expression.
-// Instead it uses a file-unique variable name.
-#define BW_ASSIGN_IMPL(EXPRESSION_THAT_RETURNS_RESULTT, ...)                                                           \
-    auto BW_UNIQUE_NAME(assignOrErrorResult, __LINE__) = EXPRESSION_THAT_RETURNS_RESULTT;                              \
-    if (!BW_UNIQUE_NAME(assignOrErrorResult, __LINE__)) [[unlikely]] {                                                 \
-        babelwiresOnError();                                                                                           \
-        return std::unexpected(BW_UNIQUE_NAME(assignOrErrorResult, __LINE__).error());                                 \
-    }                                                                                                                  \
-    __VA_ARGS__ = std::move(*BW_UNIQUE_NAME(assignOrErrorResult, __LINE__));
+// Helper macros.
+#include <BaseLib/Result/resultDSLDetail.hpp>
 
 // Helper macros for working with Results. These are designed to be used in functions that return a Result or ResultT,
 // and allow for easy propagation of errors without needing to write boilerplate code. See ResultTests.cpp for examples.
